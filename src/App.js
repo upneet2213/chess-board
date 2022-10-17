f15f46ea2aa994ff6ef48328fc965c333bf0cf21
Upneet Singh <upneet.s@freshflows.io>
2022-10-17 20:07:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isSelecting, setIsSelecting] = useState(true);
+  const [selected, setSelected] = useState(null);
+  const [positions, setPositions] = useState({ 0: [0, 0] });
+  const rowArray = [];
+  for (let i = 0; i < 8; i++) {
+    rowArray[i] = [];
+    for (let j = 0; j < 8; j++) {
+      if (j === 0) {
+      }
+      rowArray[i].push(j);
+    }
+  }
+  const handleClick = (row, cell) => {
+    if (isSelecting) {
+      for (const position in positions) {
+        if (
+          JSON.stringify(positions[position]) === JSON.stringify([row, cell])
+        ) {
+          setSelected(position);
+          setIsSelecting(false);
+          return;
+        }
+      }
+    }
+    const newPositions = positions;
+    newPositions[selected] = [row, cell];
+    setPositions(newPositions);
+    setIsSelecting(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {rowArray.map((row, index) => {
+        return (
+          <div className="row">
+            {row.map((cell) => {
+              return (
+                <div
+                  className={`cell ${
+                    JSON.stringify(positions[selected]) ===
+                    JSON.stringify([index, cell])
+                      ? "selected"
+                      : ""
+                  }`}
+                  onClick={() => handleClick(index, cell)}
+                ></div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
